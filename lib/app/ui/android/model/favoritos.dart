5f1d43119d35a31b1893959f06524508bed03f48
Ruan Dutra Braga <ruandutrab@users.noreply.github.com>
2021-04-21 14:19:48 -0300

    Update favoritos.dart

--- lib/app/ui/android/model/favoritos.dart
+++ lib/app/ui/android/model/favoritos.dart
@@ -4,24 +4,49 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-// ignore: unused_element
-var favorite;
-var favoriteTrue;
-final HomeController _homeController = Get.find<HomeController>();
-final _collection1 = FirebaseFirestore.instance.collection('users').snapshots();
-final _collection2 =
-    FirebaseFirestore.instance.collection('animes_list').snapshots();
-String idAnime;
-
 class FavoritoPage extends StatefulWidget {
   @override
   _FavoritoPageState createState() => _FavoritoPageState();
 }
 
 class _FavoritoPageState extends State<FavoritoPage> {
+  final HomeController _homeController = Get.find<HomeController>();
+  List<String> animes = [];
   @override
+  void initState() {
+    var userFavorite;
+    Future<void> getFavorite() async {
+      //query the user photo
+      FirebaseFirestore.instance
+          .collection("users")
+          .doc('${_homeController.userModel.email}')
+          .snapshots()
+          .listen((event) {
+        setState(() {
+          userFavorite = event.get("favorite");
+          // print(userFavorite);
+          userFavorite.forEach((key, value) {
+            if (value == true) {
+              animes.add(key);
+            }
+          });
+        });
+      });
+    }
+
+    super.initState();
+    setState(() {
+      getFavorite();
+    });
+  }
+
+  final animeDb =
+      FirebaseFirestore.instance.collection('animes_list').snapshots();
+
   @override
   Widget build(BuildContext context) {
+    var index = 0;
+    var animesLength = animes.length - 1;
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: PreferredSize(
@@ -29,70 +54,88 @@ class _FavoritoPageState extends State<FavoritoPage> {
         child: AppBar(
           backgroundColor: Colors.blue[500],
           title: Text(
-            'Animes mais visto',
+            'Favoritos',
             style: TextStyle(fontStyle: FontStyle.italic),
           ),
         ),
       ),
       body: StreamBuilder(
-        stream: _collection1,
-        builder:
-            (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshotUser) {
-          return StreamBuilder(
-              stream: _collection2,
-              builder: (BuildContext context,
-                  AsyncSnapshot<QuerySnapshot> snapshotAnimes) {
-                if (!snapshotUser.hasData) return const Text('Loading...');
-                if (!snapshotAnimes.hasData) return const Text('Loading...');
-                return ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: snapshotAnimes.data.docs.length,
-                  itemBuilder: (BuildContext context, int i) {
-                    for (var item in snapshotUser.data.docs) {
-                      favorite = item.data()['favorite'];
-                    }
+        stream: animeDb,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('Error Inesperado ${snapshot.error}'),
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircularProgressIndicator(),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text('Carregando...')
+                ],
+              ),
+            );
+          }
+          if (!snapshot.hasData) {
+            return Center(
+              child: Column(
+                children: [
+                  CircularProgressIndicator(),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text('Aguardando dados...')
+                ],
+              ),
+            );
+          }
 
-                    favorite.forEach((key, value) {
-                      // print('index=$key, value=$value');
-                      if (value == true) {
-                        favoriteTrue = key;
-                      }
-                    });
+          return ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: snapshot.data.docs.length,
+            itemBuilder: (BuildContext context, int i) {
+              var item = snapshot.data.docs[i];
+              var items = snapshot.data.docs[i].id;
+              index = 0;
 
-                    print(favoriteTrue);
+              if (items != animes[index]) {
+                while ((items != animes[index]) & (index < animesLength)) {
+                  index++;
+                }
+              }
 
-                    var item = snapshotAnimes.data.docs[i];
-
-                    return Container(
-                      // color: Colors.grey[200],
-                      child: Row(
-                        // crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          GestureDetector(
-                            onTap: () {
-                              String id = item.id;
-                              // Captura o documento que foi clicado
-                              FirebaseFirestore.instance
-                                  .collection('animes_list')
-                                  .doc(item.id)
-                                  .update({
-                                'views': FieldValue.increment(1),
-                              });
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => HomeAnime(
-                                            idAnime: id,
-                                            nomeAnime: item.data()['nome'],
-                                            imgCard: item.data()['img_card'],
-                                            releaseYear:
-                                                item.data()['release_date'],
-                                            description:
-                                                item.data()['description'],
-                                            completed: item.data()['completed'],
-                                          )));
-                            },
-                            child: Container(
+              return Container(
+                child: Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        String id = item.id;
+                        // Captura o documento que foi clicado
+                        FirebaseFirestore.instance
+                            .collection('animes_list')
+                            .doc(item.id)
+                            .update({
+                          'views': FieldValue.increment(1),
+                        });
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HomeAnime(
+                                      idAnime: id,
+                                      nomeAnime: item.data()['nome'],
+                                      imgCard: item.data()['img_card'],
+                                      releaseYear: item.data()['release_date'],
+                                      description: item.data()['description'],
+                                      completed: item.data()['completed'],
+                                    )));
+                      },
+                      child: items == animes[index]
+                          ? Container(
                               padding: EdgeInsets.all(3),
                               child: Stack(
                                 alignment: Alignment.bottomCenter,
@@ -122,14 +165,14 @@ class _FavoritoPageState extends State<FavoritoPage> {
                                   ),
                                 ],
                               ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                );
-              });
+                            )
+                          : Container(),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
         },
       ),
     );
